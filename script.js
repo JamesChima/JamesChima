8bf4bb1aeb65990db7c2e3b347fb67c307702bde
JamesChima <chima.mjames@hotmail.com>
2020-03-25 11:24:03 +0100

    added initial JavaScript file

---
+++ script.js
@@ -0,0 +1,15 @@
+let myName = "James Chima";
+let myCourses = ["HTML", "CSS", "JavaScript"];
+
+console.log(myName);
+console.log(myCourses);
+
+if (myCourses.length%2 != 0){
+  for(i=0; i<=200; i++){
+    let x = i%2;
+    if(x != 0){
+      console.log(i);
+    }
+  }
+} 
+
